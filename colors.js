493ed6ff2f981b72d6d204bdb5768c2449ae0ee5
TMDStudios <tmdstudioscontact@gmail.com>
2022-06-12 13:10:18 -0400

    Added color and size controls

--- colors.js
+++ colors.js
@@ -1,6 +1,8 @@
 var active = false;
 var blocks = 0;
 var blockNum = 0;
+var colors = 16;
+var size = 16;
 
 generateBlock();
 
@@ -12,7 +14,7 @@ function activate(){
 function changeColor(){
     const colorNum = Math.floor(Math.random()*blockNum);
     const colorBlock = document.getElementById("block"+colorNum);
-    const colorChoice = Math.floor(Math.random()*16);
+    const colorChoice = Math.floor(Math.random()*colors);
     if(colorBlock.style.backgroundColor!=="yellow"){
         colorBlock.style = "background-color: "+getColor(colorChoice)+"; width: 16px; height: 16px;";
     }
@@ -20,19 +22,38 @@ function changeColor(){
 }
 
 function generateBlock(){
-    for(var i = 1; i<17; i++){
+    for(var i = 1; i<=size; i++){
         const rootParent = document.getElementById("container");
         const rootChild = document.createElement("div");
         rootChild.className = "row";
         rootParent.appendChild(rootChild);  
-        for(var j = 1; j<17; j++){
+        for(var j = 1; j<=size; j++){
             const child = document.createElement("div");
-            const colorChoice = Math.floor(Math.random()*16);
+            const colorChoice = Math.floor(Math.random()*colors);
             child.style = "background-color: "+getColor(colorChoice)+"; width: 16px; height: 16px;";
             child.id = "block"+blockNum;
             blockNum++;
             rootChild.appendChild(child);  
         }
+    }
+}
+
+function regenerateBlock(){
+    blockNum = 0;
+    colors = document.getElementById("colors").value;
+    size = document.getElementById("size").value;
+    if(colors<2||colors===null){colors=2;}
+    if(colors>16){colors=16;}
+    if(size<4||size===null){size=4;}
+    if(size>64){size=64;}
+    const rootParent = document.getElementById("container");
+    removeChildren(rootParent);
+    generateBlock();
+}
+
+const removeChildren = (parent) => {
+    while(parent.lastChild){
+        parent.removeChild(parent.lastChild);
     }
 }
 
