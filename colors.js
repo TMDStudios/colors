486dd2d0a687dacc7d2a05705a11a54a06df6e6f
TMDStudios <tmdstudioscontact@gmail.com>
2022-06-11 23:02:24 -0400

    Initial commit

---
+++ colors.js
@@ -0,0 +1,95 @@
+var shuffle = false;
+var blocks = 0;
+var blockNum = 0;
+
+function activate(){
+    shuffle=!shuffle;
+    //for(var i = 1; i<5; i++){generateBlock();}
+    generateBlock();
+    //if(shuffle){newColors();}
+}
+
+function newColors(){
+    //const children = document.getElementById("row").children.length;
+    //console.log("children - "+children);
+    for(var i = 1; i<blockNum; i++){
+        const colorBlock = document.getElementById("block"+i).className;
+        const colorChoice = Math.floor(Math.random()*16);
+        colorBlock.style = "background-color: "+getColor(colorChoice)+"; width: 16px; height: 16px;";
+        console.log("Updated block number "+i);
+    }
+    //if(shuffle){setTimeout(newColors, 1000);}
+}
+
+function changeColor(){
+    const colorNum = Math.floor(Math.random()*blockNum);
+    //console.log("colorNum = "+colorNum);
+    const colorBlock = document.getElementById("block"+colorNum);
+    //console.log("colorBlock = "+colorBlock);
+    const colorChoice = Math.floor(Math.random()*16);
+    colorBlock.style = "background-color: "+getColor(colorChoice)+"; width: 16px; height: 16px;";
+    
+    setTimeout(changeColor, 50);
+}
+
+function generateBlock(){
+    for(var i = 1; i<5; i++){
+        const rootParent = document.getElementById("container");
+        const rootChild = document.createElement("div");
+        rootChild.className = "row";
+        rootParent.appendChild(rootChild);  
+        for(var j = 1; j<17; j++){
+            const child = document.createElement("div");
+            const colorChoice = Math.floor(Math.random()*16);
+            child.style = "background-color: "+getColor(colorChoice)+"; width: 16px; height: 16px;";
+            child.id = "block"+blockNum;
+            blockNum++;
+            rootChild.appendChild(child);  
+        }
+    }
+    if(blocks<3){
+        blocks++;
+        setTimeout(generateBlock, 100);
+    }else{
+        setTimeout(changeColor, 50);
+    }
+}
+
+function getColor(colorNumber){
+    switch(colorNumber){
+        case 0:
+            return "black"
+        case 1:
+            return "silver"
+        case 2:
+            return "gray"
+        case 3:
+            return "white"
+        case 4:
+            return "maroon"
+        case 5:
+            return "red"
+        case 6:
+            return "purple"
+        case 7:
+            return "fuchsia"
+        case 8:
+            return "green"
+        case 9:
+            return "lime"
+        case 10:
+            return "olive"
+        case 11:
+            return "yellow"
+        case 12:
+            return "navy"
+        case 13:
+            return "blue"
+        case 14:
+            return "teal"
+        case 15:
+            return "aqua"
+        default:
+            return "black"
+    }
+}
